f84afc3e253cc3b95fabe42080a2cf50dceb9455
Marc Mauger <simianarmy@gmail.com>
2013-09-14 16:14:26 -0700

    Update ghosthorse_whisperer.rb

--- ghosthorse_whisperer.rb
+++ ghosthorse_whisperer.rb
@@ -38,7 +38,7 @@ end.parse!
 Twitter.configure do |config|
   config.consumer_key = ''
   config.consumer_secret = ''
-  config.oauth_token = '54721863-'
+  config.oauth_token = ''
   config.oauth_token_secret = ''
 end
 
